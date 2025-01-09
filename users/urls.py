43c881001ee386ace7c15fc5152e39e5f02794e0
Juuso Silén <144767246+JSilen@users.noreply.github.com>
2025-01-09 18:54:18 +0200

    Add registration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
 ]
